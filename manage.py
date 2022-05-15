c73839de4b1f1e4a2a54e715337cf16aaed28ab8
96olbap <alvinwanjala@gmail.com>
2022-05-15 21:57:04 +0300

    This commit will initialize migrations and run the initial migration

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 from app import create_app,db
 from flask_script import Manager,Server
-
-from app.models import User
+from flask_migrate import Migrate,MigrateCommand
+from app.models import User,Blog_post,Comments
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
+migrate = Migrate(app,db)
+
 manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
@@ -20,7 +23,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User)
+    return dict(app = app, db = db, User = User, Blog_post = Blog_post, Comments = Comments)
 
 if __name__ == '__main__':
     manager.run()
