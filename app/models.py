f59ac2873c140111d2d7964e27cddb741ed01563
96olbap <alvinwanjala@gmail.com>
2022-05-15 21:28:18 +0300

    This commit will create the models; user, blog_post and comments

---
+++ app/models.py
@@ -0,0 +1,60 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
+
+class User(db.Model):
+    '''
+    User class that will define user objects
+    '''
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(80), index = True)
+    email = db.Column(db.String(255),unique= True, index = True)
+    bio =db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    post = db.relationship('Blog_post', backref = 'posts', lazy = 'dynamic')
+    comment = db.relationship('Comments', backref = 'comments', lazy = 'dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User { self.username }'
+
+class Blog_post(db.Model):
+    '''
+    Blog_post class that will define post objects
+    '''
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer,primary_key = True)
+    blog_pic_path = db.Column(db.String())
+    content = db.Column(db.String(255), nullable = False)
+    author = db.Column(db.String(80), nullable = False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    category = db.Column(db.String(), nullable = False)
+    like = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comment = db.relationship('Comments', backref = 'comments', lazy = 'dynamic')
+
+class Comments(db.Model):
+    '''
+    Comments class to define coment objects
+    '''
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer,primary_key = True)
+    content = db.Column(db.String(255), nullable = False)
+    posted_at = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+    blog_post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'User {self.content}'
