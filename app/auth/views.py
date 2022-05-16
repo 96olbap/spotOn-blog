22a36eaea26a4ad0c1184e8692ea7e76459ccc2a
96olbap <alvinwanjala@gmail.com>
2022-05-16 12:11:42 +0300

    This commit will create the register view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,27 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash
 from . import auth
 from ..models import User
 from .forms import RegistrationForm
 from .. import db
+
+# Registration view function
+@auth.route('/register',methods = ['GET', 'POST'])
+def register():
+    '''
+    register function to register new users and add them into the database.
+    '''
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'New account setup for {form.username.data}')
+
+        return redirect(url_for('auth.login'))
+        title = 'New Account'
+    return render_template('auth/register.html', registration_form = form)
+
 
 @auth.route('/login')
 def login():
