2cca9bb6579245b037bf1767f8354d9acea845b7
96olbap <alvinwanjala@gmail.com>
2022-05-16 13:19:46 +0300

    This commit will update login and refister template files and update forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,7 +7,7 @@ from wtforms import ValidationError
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[InputRequired(),Email()], render_kw={"placeholder": "example@example.com"})
     username = StringField('Enter your username',validators = [InputRequired()], render_kw={"placeholder": "Username"})
-    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')], render_kw={"placeholder": "**********"})
+    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')], render_kw={"placeholder": "Set a password"})
     password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()], render_kw={"placeholder":"Confirm Password"})
     submit = SubmitField('Sign Up')
 
