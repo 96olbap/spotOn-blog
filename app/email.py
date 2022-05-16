1b39c70085e2b10a18e2399393167d90f793a305
96olbap <alvinwanjala@gmail.com>
2022-05-16 22:05:04 +0300

    This commit will update email functionality

--- app/email.py
+++ app/email.py
@@ -2,11 +2,11 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-def mail_message(subject,template,to,**kwargs):
+
+def mail_message(subject, template, to, **kwargs):
     sender_email = 'snrdev69@gmail.com'
 
-    email = Message(subject, sender=sender_email, receipients=[to])
-    email.body = render_template(template + '.txt', **kwargs)
-    email.html = render_template(template + '.html', **kwargs)
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body = render_template(template + ".txt",**kwargs)
+    email.html = render_template(template + ".html",**kwargs)
     mail.send(email)
-    
