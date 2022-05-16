5bf5447be31d7b1f18dccfd3c8c37df71ad035a3
96olbap <alvinwanjala@gmail.com>
2022-05-16 12:41:40 +0300

    This commit will update login view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,9 @@
-from flask import render_template,redirect,url_for,flash
+from turtle import title
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm,RegistrationForm
 from .. import db
 
 # Registration view function
@@ -19,12 +21,27 @@ def register():
         flash(f'New account setup for {form.username.data}')
 
         return redirect(url_for('auth.login'))
-        title = 'New Account'
-    return render_template('auth/register.html', registration_form = form)
+    title = 'New Account'
+    return render_template('auth/register.html', registration_form = form, title=title)
 
 
 @auth.route('/login')
 def login():
-    
+    '''
+    login view function to login a registered user
+    '''
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
 
-    return render_template('auth/login.html')
+            flash(f'Hello, welcome back')
+
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "User login"
+
+    return render_template('auth/login.html',login_form = login_form,title=title)
